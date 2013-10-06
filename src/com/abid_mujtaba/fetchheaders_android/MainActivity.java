e16405159b0e3109746d7e05adea28596f2b8c43
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2013-10-05 20:08:46 -0500

    Adds skeletal menu item that is supposed to allow one to access the Accounts settings.

--- src/com/abid_mujtaba/fetchheaders_android/MainActivity.java
+++ src/com/abid_mujtaba/fetchheaders_android/MainActivity.java
@@ -2,14 +2,46 @@ package com.abid_mujtaba.fetchheaders_android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.menu_accounts:
+
+                Toast.makeText(this, "Accounts!", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
